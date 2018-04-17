308f25530f68ac71ef52ec61469b394918b3e831
Vishal Sharma <vsh1819@gmail.com>
2018-04-17 14:56:37 +0100

    added a schema and validators to the model

--- server/server.js
+++ server/server.js
@@ -5,22 +5,50 @@ mongoose.connect('mongodb://localhost:27017/TodoApp')
 
 let Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   complete: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 })
 
-let newTodo = new Todo({
-  text: 'Cook dinner'
+
+// let otherTodo = new Todo({
+//   text: '    Edited this video '
+//   // text: 'Feed the cat',
+//   // completed: true,
+//   // completedAt: 123
+// })
+//
+// otherTodo.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save', e);
+// })
+
+let User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
 })
 
-newTodo.save().then((doc) => {
-  console.log('Saved todo', doc);
+let user = new User({
+  email: 'andrew@example.com   '
+})
+
+user.save().then((doc) => {
+  console.log('User saved', doc);
 }, (e) => {
-  console.log('Unable to save todo');
+  console.log('Unable to save user', e);
 })
