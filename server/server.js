3aafa5486dcda4ae727e6755b06d0a0eb36e141a
Vishal Sharma <vsh1819@gmail.com>
2018-04-18 10:57:06 +0100

    setup app

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ let {Todo} = require('./models/todo')
 let {User} = require('./models/user')
 
 let app = express()
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 
@@ -70,8 +71,8 @@ app.get('/todos/:id', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
 })
 
 module.exports = {app}
