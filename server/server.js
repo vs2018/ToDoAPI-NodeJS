9003a8cfe854975f5ba8de4d8a966fb256e51662
Vishal Sharma <vsh1819@gmail.com>
2018-04-18 12:13:03 +0100

    add delete /todos/:id route

--- server/server.js
+++ server/server.js
@@ -33,21 +33,6 @@ app.get('/todos', (req, res) => {
   })
 })
 
-// app.post('/todos/users', (req, res) => {
-//   console.log(req.body.text);
-//   let user = new User({
-//     email: req.body.text
-//   })
-//
-//   console.log(user);
-//
-//   user.save().then((doc) => {
-//     res.send(doc)
-//   }, (e) => {
-//     res.status(400).send(e)
-//   })
-// })
-
 app.get('/todos/:id', (req, res) => {
   let id = req.params.id
 
@@ -65,9 +50,24 @@ app.get('/todos/:id', (req, res) => {
     res.status(400).send()
   })
 
+})
 
+app.delete('/todos/:id', (req, res) => {
+  let id = req.params.id
 
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
 
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send()
+    }
+
+    res.send({todo})
+  }).catch((e) => {
+    res.status(400).send()
+  })
 })
 
 
